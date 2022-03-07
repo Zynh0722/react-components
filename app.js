728260c75f70d705114dfae3684193907007cf9c
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-07 11:29:37 -0800

    Finish Exercize

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // TODO
+// var GroceryListItem = (props) => (
+//   <li onClick={props.onClick}>{props.item}</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  };
+
+  render() {
+    var style = {
+      'user-select': 'none',
+      cursor: 'pointer',
+      width: 'fit-content',
+      'font-weight': this.state.done ? 'bold' : 'normal'
+    }
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  };
+}
+
+var GroceryList = (props) => (
+  <div>
+    <h1>GroceryList</h1>
+
+    <ul>
+      {props.items.map(item => <GroceryListItem item={item} />)}
+    </ul>
+  </div>
+);
+
+
+ReactDOM.render(<GroceryList items={['Milk', 'Cookies', 'Rice']} />, document.getElementById('app'));
